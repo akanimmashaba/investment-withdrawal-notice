02619872fcecf966623e3c7708b64b75fb697362
akani <akanimmashaba@gmail.com>
2024-01-29 16:35:00 +0200

    added InvestorControllers

--- src/main/java/com/enviro/assessment/grad001/akanimashaba/controller/InvestorController.java
+++ src/main/java/com/enviro/assessment/grad001/akanimashaba/controller/InvestorController.java
@@ -1,11 +1,53 @@
 package com.enviro.assessment.grad001.akanimashaba.controller;
+import com.enviro.assessment.grad001.akanimashaba.model.Investor;
+import com.enviro.assessment.grad001.akanimashaba.service.InvestorService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/api/investors")
 public class InvestorController {
+    @Autowired
+    private InvestorService investorService;
+
+    @GetMapping
+    public ResponseEntity<List<Investor>> getAllInvestors() {
+        List<Investor> investors = investorService.getAllInvestors();
+        return new ResponseEntity<>(investors, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Investor> getInvestorById(@PathVariable Long id) {
+        Optional<Investor> investor = investorService.getInvestorById(id);
+        return investor.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Investor> createInvestor(@RequestBody Investor investor) {
+        Investor createdInvestor = investorService.createInvestor(investor);
+        return new ResponseEntity<>(createdInvestor, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Investor> updateInvestor(@PathVariable Long id, @RequestBody Investor updatedInvestor) {
+        Investor updated = investorService.updateInvestor(id, updatedInvestor);
+        return updated != null ? new ResponseEntity<>(updated, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteInvestor(@PathVariable Long id) {
+        investorService.deleteInvestor(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
