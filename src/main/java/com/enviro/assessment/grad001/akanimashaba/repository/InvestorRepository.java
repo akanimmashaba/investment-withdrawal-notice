c1118620ccfd80601f212a83294f85866a8c9775
akani <akanimmashaba@gmail.com>
2024-01-29 16:34:27 +0200

    configed Repisitory interface for h2database

--- src/main/java/com/enviro/assessment/grad001/akanimashaba/repository/InvestorRepository.java
+++ src/main/java/com/enviro/assessment/grad001/akanimashaba/repository/InvestorRepository.java
@@ -2,7 +2,11 @@ package com.enviro.assessment.grad001.akanimashaba.repository;
 
 import com.enviro.assessment.grad001.akanimashaba.model.Investor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface InvestorRepository extends JpaRepository<Investor, Long> {
 
 }
