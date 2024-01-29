83fadba52c2c31a40c7ff07fea57c1e762201f1a
akani <akanimmashaba@gmail.com>
2024-01-29 16:35:31 +0200

    added ProductController, StatementController and WithdrawalController

--- src/main/java/com/enviro/assessment/grad001/akanimashaba/controller/StatementController.java
+++ src/main/java/com/enviro/assessment/grad001/akanimashaba/controller/StatementController.java
@@ -2,15 +2,49 @@ package com.enviro.assessment.grad001.akanimashaba.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.enviro.assessment.grad001.akanimashaba.model.Investor;
 import com.enviro.assessment.grad001.akanimashaba.model.Statement;
 import com.enviro.assessment.grad001.akanimashaba.service.StatementService;
-
-import java.time.LocalDate;
+import org.springframework.http.HttpStatus;
 import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/statements")
 public class StatementController {
+    @Autowired
+    private StatementService statementService;
+
+    @GetMapping
+    public ResponseEntity<List<Statement>> getAllStatements() {
+        List<Statement> statements = statementService.getAllStatements();
+        return new ResponseEntity<>(statements, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Statement> getStatementById(@PathVariable Long id) {
+        Optional<Statement> statement = statementService.getStatementById(id);
+        return statement.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Statement> createStatement(@RequestBody Statement statement) {
+        Statement createdStatement = statementService.createStatement(statement);
+        return new ResponseEntity<>(createdStatement, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Statement> updateStatement(@PathVariable Long id, @RequestBody Statement updatedStatement) {
+        Statement updated = statementService.updateStatement(id, updatedStatement);
+        return updated != null ? new ResponseEntity<>(updated, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStatement(@PathVariable Long id) {
+        statementService.deleteStatement(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
--- src/main/java/com/enviro/assessment/grad001/akanimashaba/controller/WithdrawalController.java
+++ src/main/java/com/enviro/assessment/grad001/akanimashaba/controller/WithdrawalController.java
@@ -1,6 +1,5 @@
 package com.enviro.assessment.grad001.akanimashaba.controller;
 
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +7,49 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.enviro.assessment.grad001.akanimashaba.model.WithdrawalNotice;
 import com.enviro.assessment.grad001.akanimashaba.service.WithdrawalService;
+import org.springframework.http.HttpStatus;
 
+import java.util.List;
+import java.util.Optional;
 import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/withdrawals")
 public class WithdrawalController {
+    @Autowired
+    private WithdrawalService withdrawalNoticeService;
+
+    @GetMapping
+    public ResponseEntity<List<WithdrawalNotice>> getAllWithdrawalNotices() {
+        List<WithdrawalNotice> withdrawalNotices = withdrawalNoticeService.getAllWithdrawalNotices();
+        return new ResponseEntity<>(withdrawalNotices, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WithdrawalNotice> getWithdrawalNoticeById(@PathVariable Long id) {
+        Optional<WithdrawalNotice> withdrawalNotice = withdrawalNoticeService.getWithdrawalNoticeById(id);
+        return withdrawalNotice.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<WithdrawalNotice> createWithdrawalNotice(@RequestBody WithdrawalNotice withdrawalNotice) {
+        WithdrawalNotice createdWithdrawalNotice = withdrawalNoticeService.createWithdrawalNotice(withdrawalNotice);
+        return new ResponseEntity<>(createdWithdrawalNotice, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<WithdrawalNotice> updateWithdrawalNotice(@PathVariable Long id, @RequestBody WithdrawalNotice updatedWithdrawalNotice) {
+        WithdrawalNotice updated = withdrawalNoticeService.updateWithdrawalNotice(id, updatedWithdrawalNotice);
+        return updated != null ? new ResponseEntity<>(updated, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWithdrawalNotice(@PathVariable Long id) {
+        withdrawalNoticeService.deleteWithdrawalNotice(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 
 }
